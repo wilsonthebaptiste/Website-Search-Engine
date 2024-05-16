0b3a843713d3d604397a76a2831ba04b434f5e1c
Wilson <wilsonthebaptiste@gmail.com>
2024-05-16 10:57:52 -0400

    added function to detect search button click and grab value

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,9 @@
 $(document).ready(()=>{
 
+    $('#submit').click(()=>{
+        let userInput = $('#search').val()
+        alert(userInput)
+    });
     $.ajax({
      url: 'http://www.omdbapi.com/?i=tt3896198&apikey=ec5f8eb1'   
     })
