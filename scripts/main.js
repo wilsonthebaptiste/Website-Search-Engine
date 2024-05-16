c9fe174cc7e13af4588afc4fcd2d36ea1a48fb00
Wilson <wilsonthebaptiste@gmail.com>
2024-05-16 11:13:06 -0400

    moved api call into click function

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,10 @@ $(document).ready(()=>{
     $('#submit').click(()=>{
         let userInput = $('#search').val()
         alert(userInput)
+        $.ajax({
+            url: 'http://www.omdbapi.com/?i=tt3896198&apikey=ec5f8eb1'   
+           })
     });
-    $.ajax({
-     url: 'http://www.omdbapi.com/?i=tt3896198&apikey=ec5f8eb1'   
-    })
+    
 
 })
