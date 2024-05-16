5d9de26d752b927c6a9fba1e4a451d48490deb0b
Wilson <wilsonthebaptiste@gmail.com>
2024-05-16 17:31:13 -0400

    added API key

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,16 @@
 $(document).ready(()=>{
 
     $('#submit').click(()=>{
-        let userInput = $('#search').val()
-        alert(userInput)
+        let userInput = $('#search').val();
+        console.log(userInput)
+        /*alert(userInput)*/
         $.ajax({
-            url: 'http://www.omdbapi.com/?i=tt3896198&apikey=ec5f8eb1'   
+            url: `http://www.omdbapi.com/?apikey=ec5f8eb1&s=${userInput}`   
            })
-    });
+           .done((response)=>{
+            console.log("response", response)
+        })
+    })
     
 
 })
