f7cc3c7fe273cd44cb2ebe81bd20fd1a772b2ed1
Wilson <wilsonthebaptiste@gmail.com>
2024-05-17 23:51:14 -0400

    added a function to return multple pages of movies

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,13 @@ $(document).ready(()=>{
         $("#movieList").empty();
         let userInput = $('#search').val();
         console.log(userInput)
+        getPages = (p) => {
         /*alert(userInput)*/
         $.ajax({
-            url: `http://www.omdbapi.com/?apikey=ec5f8eb1&s=${userInput}`   
+            url: `http://www.omdbapi.com/?apikey=ec5f8eb1&s=${userInput}&page=${p}`   
            })
            .done((response)=>{
+            console.log(response)
             let movies = response.Search
             let movieCount = response.totalResults
             $.each(movies, (i, e)=>{
@@ -20,7 +22,9 @@ $(document).ready(()=>{
                     $('#movieSearchNumber').val(movieCount)
                 }
             })
-        })
+        })}
+        getPages("1")
+        getPages("2")
     })
     
 
